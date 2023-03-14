0fe3af693c665b29e3a6472fe43715f2855336bd
beginoffile <cursos.mapatupisaro@gmail.com>
2023-03-13 23:45:24 -0600

    add sw

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 import './styles.css';
 import 'normalize.css/normalize.css';
+
+
+if ('serviceWorker' in navigator){
+    window.addEventListener('load', () =>{
+        navigator.serviceWorker.register('./service-worker.js').then(registration =>{
+            console.log('SW registered', registration)
+        }).catch(registrationError =>{
+            console.log('Sw Registration failed', registrationError);
+        });
+    })
+}
